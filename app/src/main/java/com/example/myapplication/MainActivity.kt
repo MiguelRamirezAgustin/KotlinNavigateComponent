39870df2df4c10bdda7b7fff7246d3900dede2e3
MiguelRamirezAgustin <40777839+MiguelRamirezAgustin@users.noreply.github.com>
2024-07-01 13:47:30 -0600

    se crea navegacion a otp

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,49 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var visibleSearch: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-       /* val otpEditTexts = arrayOf(
-            findViewById<EditText>(R.id.et_otp_1),
-            findViewById<EditText>(R.id.et_otp_2),
-            findViewById<EditText>(R.id.et_otp_3),
-            findViewById<EditText>(R.id.et_otp_4),
-            findViewById<EditText>(R.id.et_otp_5),
-            findViewById<EditText>(R.id.et_otp_6)
-        )
-
-        for (i in otpEditTexts.indices) {
-            otpEditTexts[i].addTextChangedListener(object : TextWatcher {
-                override fun beforeTextChanged(
-                    s: CharSequence?,
-                    start: Int,
-                    count: Int,
-                    after: Int
-                ) {
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    if (s?.length == 1 && i < otpEditTexts.size - 1) {
-                        otpEditTexts[i + 1].requestFocus()
-                    } else if (s?.length == 0 && i > 0) {
-                        otpEditTexts[i - 1].requestFocus()
-                    }
-                    val opt = otpEditTexts.get(0).text.toString()+otpEditTexts.get(1).text.toString()+otpEditTexts.get(2).text.toString()+otpEditTexts.get(3).text.toString()+otpEditTexts.get(4).text.toString()+otpEditTexts.get(5).text.toString()
-                    Log.d("Otp", "GetOpt "+opt )
-                }
-
-                override fun afterTextChanged(s: Editable?) {
-
-                    Log.d("Otp", "GetOptAfter ${s}")
-                }
-            })
-        }*/
 
     }
 }
---
+++ app/src/main/java/com/example/myapplication/MenuFragment.kt
@@ -0,0 +1,45 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentMenuBinding
+
+class MenuFragment : Fragment() {
+
+    private var _binding: FragmentMenuBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() = with(binding) {
+        btnOtp.setOnClickListener {
+            findNavController().navigate(R.id.action_menuFragment_to_otpFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
